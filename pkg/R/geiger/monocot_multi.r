a4c41d1509feceb338a2557d4b922e513505ac0b
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2012-02-07 15:56:41 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phylooptim@358 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/geiger/monocot_multi.r
+++ pkg/R/geiger/monocot_multi.r
@@ -225,7 +225,7 @@ lt <- vector("list",length(well))
 h <- 1
   for (i in c(1:N)){
     for (j in c(1:length(v[[i]][,1]))){
-lt[[h]]<-c(unlist(v[[i]][j]),round(as.numeric(results[[i]][[j]]$lnl),5),round(as.numeric(results[[i]][[j]]$beta.start),5),round(as.numeric(results[[i]][[j]]$beta.bnd[1]),5),round(as.numeric(results[[i]][[j]]$beta.bnd[2]),5),round(results[[i]][[j]]$beta,5),round(as.numeric(results[[i]][[j]]$delta.start),5),round(as.numeric(results[[i]][[j]]$delta.bnd[1]),5),round(as.numeric(results[[i]][[j]]$delta.bnd[2]),5),round(results[[i]][[j]]$delta,5),round(results[[i]][[j]]$time,5));h <- h+1}}
+lt[[h]]<-c(unlist(v[[i]][j]),round(as.numeric(results[[i]][[j]]$lnl),5),round(as.numeric(results[[i]][[j]]$beta.start),5),round(as.numeric(exp(results[[i]][[j]]$beta.bnd[1])),5),round(exp(as.numeric(results[[i]][[j]]$beta.bnd[2])),5),round(results[[i]][[j]]$beta,5),round(as.numeric(results[[i]][[j]]$delta.start),5),round(exp(as.numeric(results[[i]][[j]]$delta.bnd[1])),5),round(exp(as.numeric(results[[i]][[j]]$delta.bnd[2])),5),round(results[[i]][[j]]$delta,5),round(results[[i]][[j]]$time,5));h <- h+1}}
 
 ff <- data.frame(matrix(NA,ncol=length(lt[[1]]),nrow=length(lt)))
 colnames(ff)<-c("name","L","bLB","bUB","b","dLB","dUB","d","time")
@@ -234,12 +234,27 @@ for (i in c(1:length(lt))){
     ff[i,j] <- lt[[i]][j]}}
 ff <- ff[order(ff$name,ff$dUB) , ]
 
-l <- vector("list",length(well))
-names(l) <- c("BFGS","bobyqa","CG","L-BFGS-B","Nelder-Mead","newuoa","nlm","nlminb","Rcgmin","Rvmmin","spg","ucminf")
+ll <- vector("list",length(well))
+names(ll) <- c("BFGS","bobyqa","CG","L-BFGS-B","Nelder-Mead","newuoa","nlm","nlminb","Rcgmin","Rvmmin","spg","ucminf")
 
 aa <- seq(1,it*z,by=it)
 bb <- seq(aa[2]-1,it*z,by=it)
-for (i in c(1:length(aa))){l[[i]] <- ff[aa[i]:bb[i],2:9]}
+for (i in c(1:length(aa))){ll[[i]] <- ff[aa[i]:bb[i],2:9]}
+
+l <- vector("list",length(well))
+names(l) <- well
+l[[1]] <- ll[[11]]
+l[[2]] <- ll[[9]]
+l[[3]] <- ll[[10]]
+l[[4]] <- ll[[2]]
+l[[5]] <- ll[[4]]
+l[[6]] <- ll[[8]]
+l[[7]] <- ll[[12]]
+l[[8]] <- ll[[5]]
+l[[9]] <- ll[[7]]
+l[[10]] <- ll[[3]]
+l[[11]] <- ll[[1]]
+l[[12]] <- ll[[6]]
 
 save.image("/Users/michels/phylooptim/pkg/R/geiger/mamgeigererror.RData")
 
