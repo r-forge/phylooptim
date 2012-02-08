8faa33c07252027d3a5c51e4e52c5f02893c87df
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2012-02-08 21:06:26 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phylooptim@395 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/geiger/geiger.multi.R
+++ pkg/R/geiger/geiger.multi.R
@@ -4,13 +4,38 @@ require(ape)
 require(multicore)
 load("geigerbackup.RData")
 
-#Run this
-M <- 22
-it <- 50  	                         #Number of iterations
+#The following will detect the number of cores you have.
+if(.Platform$OS.type == "windows") {
+detectCores <- function(all.tests = FALSE)
+    .Call("ncpus", FALSE, package = "parallel")
+} else {
+detectCores <- function(all.tests = FALSE)
+{
+    systems <-
+        list(darwin  = "/usr/sbin/sysctl -n hw.ncpu 2>/dev/null",
+             freebsd = "/sbin/sysctl -n hw.ncpu 2>/dev/null",
+             linux   = "grep processor /proc/cpuinfo 2>/dev/null | wc -l",
+             irix    = c("hinv | grep Processors | sed 's: .*::'",
+                         "hinv | grep '^Processor '| wc -l"),
+             solaris = "/usr/sbin/psrinfo -p")
+    for (i in seq(systems))
+        if(all.tests ||
+           length(grep(paste("^", names(systems)[i], sep=''), R.version$os)))
+            for (cmd in systems[i]) {
+                a <- gsub("^ +","", system(cmd, TRUE)[1])
+                if (length(grep("^[1-9]", a))) return(as.integer(a))
+            }
+    NA_integer_
+}
+}
+
+M <- detectCores()                      #Use if you want all cores used (Total # of cores)
+#M <- 2                                   #Use if you want to choose the # of cores to use
+it <- 4  	                         #Number of iterations (at least 2)
 z <- length(well)                        #Number of optimizers
 MIN=2                                    #min upper value
 MAX=500                                  #max upper value
-ub <- seq(MIN,MAX,length=it)             #Random beta start values
+ub <- seq(MIN,MAX,length=it)             #Upper bound values
 N <- min(it*z,M)
 
 bb <- unlist(lapply(ub, function(x) rep(x,z)))
@@ -32,18 +57,56 @@ for (d in c(1:a)){
 
 for (b in c(1:N)){v[[b]] <- na.omit(v[[b]])}
 
-#td <- treedata(read.tree("BJO.Monocot.tre"),read.delim("BJO.monocot_GS")[,3],sort=T)
-tree <- read.nexus("mammalChar1.nex")
-tree$edge.length[tree$edge.length<1e-5]=1e-5
-td <- treedata(tree,read.csv("mammallogChar1.csv", row.names=1),sort=T)
-ntax=length(td$phy$tip.label)
-chdata <- read.csv("mammallogChar1.csv")[,2]
-tree<- td$phy# Tree
-n<- length(chdata)
+##Geospiza Data
+data(geospiza)
+name <- c("geo")
+tree <- geospiza$geospiza.tree
+a <- geospiza$geospiza.data
+
+##Which column of data do you want?
+jj <- 1
+
+##Aquilegia Data
+#name <- c("aqui")
+#tree <- read.tree("Aquilegia.new.tre")
+#a <- read.delim("Aquilegia.traits",row.names=1)
+
+##Which column of data do you want?
+#jj <- 2
+
+##Monocot Data
+#name <- c("mono")
+#tree <- read.tree("BJO.Monocot.tre")
+#a <- read.delim("BJO.monocot_GS",row.names=1)
+
+##Which column of data do you want?
+#jj <- 2
+
+##Mammal Data
+#name <- c("mam")
+#tree<-read.nexus("mammalChar1.nex")
+#a <- read.csv("mammallogChar1.csv", row.names=1)
+
+##Which column of data do you want?
+#jj <- 1
 
 #----- MINIMIZE NEGATIVE LOG LIKELIHOOD
 
-start=log(c(1.5, 0.1))
+trait <- data.frame(T=a[,jj])
+rownames(trait) <- rownames(a)
+name <- c("mono")
+tree$edge.length[tree$edge.length<1e-5]=1e-5
+nc <- name.check(tree,a)
+if (nc[1]=="OK"){nc$Tree.not.data <- NULL}
+tree <- drop.tip(tree,nc$Tree.not.data)
+td <- treedata(tree,trait,sort=T)
+
+chdata<- a[,jj] # TIP data
+ntax=length(td$phy$tip.label)
+tree<- td$phy# Tree
+n <- length(chdata)
+
+start=log(c(0.5, 1.5))
 
 dplot<-TRUE
 foostore=list(x1=NULL,x2=NULL)
@@ -53,17 +116,16 @@ b.time <-proc.time()
 jobs <- lapply(v, function(x) parallel(f(x),silent=TRUE))
 results <- collect(jobs,wait=TRUE)
 total.time <- as.numeric(proc.time()[3]-b.time[3])/(60)
-#what?
 
 lt <- vector("list",length(well))
 
 h <- 1
   for (i in c(1:N)){
     for (j in c(1:length(v[[i]][,1]))){
-lt[[h]]<-c(unlist(v[[i]][j]),round(as.numeric(results[[i]][[j]]$lnl),5),round(as.numeric(results[[i]][[j]]$beta.start),5),round(as.numeric(exp(results[[i]][[j]]$beta.bnd[1])),5),round(exp(as.numeric(results[[i]][[j]]$beta.bnd[2])),5),round(results[[i]][[j]]$beta,5),round(as.numeric(results[[i]][[j]]$delta.start),5),round(exp(as.numeric(results[[i]][[j]]$delta.bnd[1])),5),round(exp(as.numeric(results[[i]][[j]]$delta.bnd[2])),5),round(results[[i]][[j]]$delta,5),round(results[[i]][[j]]$time,5));h <- h+1}}
+lt[[h]]<-c(unlist(v[[i]][j]),round(as.numeric(results[[i]][[j]]$lnl),5),round(as.numeric(results[[i]][[j]]$beta.start),5),round(as.numeric(exp(results[[i]][[j]]$beta.bnd[1])),5),round(exp(as.numeric(results[[i]][[j]]$beta.bnd[2])),5),round(results[[i]][[j]]$beta,5),round(as.numeric(results[[i]][[j]]$delta.start),5),round(exp(as.numeric(results[[i]][[j]]$delta.bnd[1])),5),round(exp(as.numeric(results[[i]][[j]]$delta.bnd[2])),5),round(results[[i]][[j]]$delta,5),as.numeric(results[[i]][[j]]$conv),round(results[[i]][[j]]$time,5));h <- h+1}}
 
 ff <- data.frame(matrix(NA,ncol=length(lt[[1]]),nrow=length(lt)))
-colnames(ff)<-c("name","L","bLB","bUB","b","dLB","dUB","d","time")
+colnames(ff)<-c("name","L","bLB","bUB","b","dLB","dUB","d","conv","time")
 for (i in c(1:length(lt))){
   for (j in c(1:length(lt[[i]]))){
     ff[i,j] <- lt[[i]][j]}}
@@ -74,7 +136,7 @@ names(ll) <- c("BFGS","bobyqa","CG","L-BFGS-B","Nelder-Mead","newuoa","nlm","nlm
 
 aa <- seq(1,it*z,by=it)
 bb <- seq(aa[2]-1,it*z,by=it)
-for (i in c(1:length(aa))){ll[[i]] <- ff[aa[i]:bb[i],2:9]}
+for (i in c(1:length(aa))){ll[[i]] <- ff[aa[i]:bb[i],2:10]}
 
 l <- vector("list",length(well))
 names(l) <- well
@@ -91,6 +153,5 @@ l[[10]] <- ll[[3]]
 l[[11]] <- ll[[1]]
 l[[12]] <- ll[[6]]
 
-save.image("/Users/michels/phylooptim/pkg/R/geiger/mamgeigererror.RData")
-
-save.image("/Users/omearalabguest/phylooptim/pkg/R/geiger/mamgeigererror.RData")
+#Saves the results in the directory
+save.image(paste(getwd(),"/",name,"geigererror.RData",sep=""))
