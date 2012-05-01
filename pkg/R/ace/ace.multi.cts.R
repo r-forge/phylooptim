3426f139eda0787a7bd03624bce15d8eee4ab303
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2012-05-01 16:06:40 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phylooptim@649 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ace/ace.multi.cts.R
+++ pkg/R/ace/ace.multi.cts.R
@@ -3,78 +3,17 @@ rm(list = ls())
 require(ouch)
 require(optimx)
 require(geiger)
-require(multicore)
 source("ace.mine.cts.R")
-source("f.ace.cts.R")
-
-wellt <- c("spg", "Rcgmin", "Rvmmin", "bobyqa","L-BFGS-B",1,1,1,1,1,1,1)
-well <- c("spg", "Rcgmin", "Rvmmin", "bobyqa","L-BFGS-B","nlminb","ucminf","Nelder-Mead","nlm","CG","BFGS","newuoa")
-
-#The following will detect the number of cores you have.
-if(.Platform$OS.type == "windows") {
-detectCores <- function(all.tests = FALSE)
-    .Call("ncpus", FALSE, package = "parallel")
-} else {
-detectCores <- function(all.tests = FALSE)
-{
-    systems <-
-        list(darwin  = "/usr/sbin/sysctl -n hw.ncpu 2>/dev/null",
-             freebsd = "/sbin/sysctl -n hw.ncpu 2>/dev/null",
-             linux   = "grep processor /proc/cpuinfo 2>/dev/null | wc -l",
-             irix    = c("hinv | grep Processors | sed 's: .*::'",
-                         "hinv | grep '^Processor '| wc -l"),
-             solaris = "/usr/sbin/psrinfo -p")
-    for (i in seq(systems))
-        if(all.tests ||
-           length(grep(paste("^", names(systems)[i], sep=''), R.version$os)))
-            for (cmd in systems[i]) {
-                a <- gsub("^ +","", system(cmd, TRUE)[1])
-                if (length(grep("^[1-9]", a))) return(as.integer(a))
-            }
-    NA_integer_
-}
-}
-
-#M <- detectCores()                      #Use if you want all cores used (Total # of cores)
-M <- 22                                  #Use if you want to choose the # of cores to use
-it <- 22  	                         #Number of iterations (at least 2)
-z <- length(well)                        #Number of optimizers
-MIN=0.01                                 #min upper value
-MAX=10                                   #max upper value
-strt <- seq(MIN,MAX,length=it)           #Start values
-N <- min(M,it)
-
-a <- ceiling(it / N)
-h <- 1
-m <- 1
-v <- vector("list",N)
-for (b in c(1:N)){v[[b]]<-matrix(NA,nrow=a,ncol=1)}
-for (d in c(1:a)){
-  for (j in c(1:N)){
-    if (m > it*z) {break}else{
-    if (h <= 12){
-      if (j < N ){v[[j]][d] <- c(strt[m]);h <- h+1;m <- m+1}else{v[[j]][d] <- c(strt[m]);h<-h+1;m <- m+1;next}
-                 }else{
-      h <- 1
-      if (j < N ){v[[j]][d] <- c(strt[m]);h <- h+1;m <- m+1}else{v[[j]][d] <- c(strt[m]);h<-h+1;m <- m+1;next}}}
-  }
-}
-
-oli <- rep(NA,N)
-sum.oli <- rep(NA,N)
-for (b in c(1:N)){v[[b]] <- na.omit(v[[b]])}
-for (b in c(1:N)){oli[b] <- length(v[[b]])}
-for (b in c(1:N)){sum.oli[b] <- sum(oli[b:1])}
 
 #DATA TO BE USED (Default is geospiza)
 ##Geospiza data
-#data(geospiza)
-#name <- c("geo")
-#tree <- geospiza$geospiza.tree
-#a.trait <- geospiza$geospiza.data
+data(geospiza)
+name <- c("geo")
+tree <- geospiza$geospiza.tree
+a.trait <- geospiza$geospiza.data
 
 ##Which column of data do you want?
-#kk <- 1
+kk <- 1
 
 ##Aquilegia Data
 #name <- c("aqui")
@@ -85,12 +24,12 @@ for (b in c(1:N)){sum.oli[b] <- sum(oli[b:1])}
 #kk <- 2
 
 ##Monocot Data
-name <- c("mono")
-tree <- read.tree("BJO.Monocot.tre")
-a.trait <- read.delim("BJO.monocot_GS",row.names=1)
+#name <- c("mono")
+#tree <- read.tree("BJO.Monocot.tre")
+#a.trait <- read.delim("BJO.monocot_GS",row.names=1)
 
 ##Which column of data do you want?
-kk <- 2
+#kk <- 2
 
 ##Mammal Data
 #name <- c("mam")
@@ -115,24 +54,8 @@ a.trait <- a.trait[order(a.trait$new),]
 dv <- treedata(tree,a.trait[,kk],sort=T)
 
 b.time <-proc.time()
-jobs <- lapply(v, function(x) parallel(f(x),silent=TRUE))
-results <- collect(jobs,wait=TRUE)
+l <- ace(as.vector(dv$data),dv$phy,type='continuous',CI=FALSE)
 total.time <- as.numeric(proc.time()[3]-b.time[3])/(60)
-
-l <- vector("list",length(well))
-names(l) <- well
-for (i in c(1:length(well))){l[[i]] <- matrix(NA,ncol=7,nrow=it);colnames(l[[i]]) <- c("I","lb","ub","P","L","time","conv")}
-
-for (j in c(1:N)){
-  for (i in c(1:z)){
-    for (d in c(1:oli[j])){
-      if (j == 1 ){l[[i]][d,] <- as.numeric(results[[j]][[i]][d,])}else{
-                   l[[i]][d+sum.oli[j-1],] <- as.numeric(results[[j]][[i]][d,])}
-    }
-  }
-}
-
-for (i in c(1:z)){l[[i]] <- l[[i]][order(l[[i]][,1]) , ]}
 
 #Saves the results in the directory
 save.image(paste(getwd(),"/",name,"acects.RData",sep=""))
