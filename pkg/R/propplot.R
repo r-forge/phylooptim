3a3ac9514a99f0693f2d8ed18285a473c353021a
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2012-06-18 17:29:07 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phylooptim@784 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/propplot.R
+++ pkg/R/propplot.R
@@ -15,87 +15,7 @@ prop[[4]] <- o.table[[7]]
 rm(list=ls()[-1*which(ls()=="prop")])
 names(prop) <- c("acedisc","geigercts","geigerdisc","ouchcts")
 
-#This way has x and y flipped
-
-propnew <- matrix(NA,ncol=12,nrow=16)
-for (k in c(1:4)){
-  for (i in c(1:12)){
-    for (j in c(1:4)){
-if (k==1){propnew[j,i] <- prop[[k]][j,i+1]}
-if (k==2){propnew[4+j,i] <- prop[[k]][j,i+1]}
-if (k==3){propnew[8+j,i] <- prop[[k]][j,i+1]}
-if (k==4){propnew[12+j,i] <- prop[[k]][j,i+1]}
-    }
-  }
-}
-
-png("propplot.png")
-plot(x=c(-1,12),y=c(0,17.75),type="n",axes=FALSE,xlab="",ylab="",main="Proportion Correct")
-for (y in c(1:4)) {
-  for (x in c(1:12))  {
-     rect(xleft=x-1,xright=x,ybottom=17-y,ytop=17-(y+1),col=gray(1-propnew[y,x]))
-   }
-}
-
-for (y in c(5:8)) {
-  for (x in c(1:12))  {
-     rect(xleft=x-1,xright=x,ybottom=17-y-0.25,ytop=17-(y+1)-0.25,col=gray(1-propnew[y,x]))
-   }
-}
-
-for (y in c(9:12)) {
-  for (x in c(1:12))  {
-     rect(xleft=x-1,xright=x,ybottom=17-y-0.5,ytop=17-(y+1)-0.5,col=gray(1-propnew[y,x]))
-   }
-}
-
-for (y in c(13:16)) {
-  for (x in c(1:12))  {
-     rect(xleft=x-1,xright=x,ybottom=17-y-0.75,ytop=17-(y+1)-0.75,col=gray(1-propnew[y,x]))
-   }
-}
-
-text(.5, 16.5, "spg",cex = .8,srt=45)
-text(1.5, 16.8, "Rcgmin",cex = .8,srt=45)
-text(2.5, 16.8, "Rvmmin",cex = .8,srt=45)
-text(3.5, 16.8, "bobyqa",cex = .8,srt=45)
-text(4.7, 17.05, "L-BFGS-B",cex = .8,srt=45)
-text(5.5, 16.8, "nlminb",cex = .8,srt=45)
-text(6.5, 16.8, "ucminf",cex = .8,srt=45)
-text(7.8, 17.2, "Nelder-Mead",cex = .8,srt=45)
-text(8.5, 16.5, "nlm",cex = .8,srt=45)
-text(9.5, 16.5, "CG",cex = .8,srt=45)
-text(10.5, 16.7, "BFGS",cex = .8,srt=45)
-text(11.5, 16.8, "newuoa",cex = .8,srt=45)
-
-text(-.5, 15.5, "Geo",cex = .8)
-text(-.5, 14.5, "Aqui",cex = .8)
-text(-.5, 13.5, "Mono",cex = .8)
-text(-.5, 12.5, "Mam",cex = .8)
-
-text(-.5, 11.5-.25, "Geo",cex = .8)
-text(-.5, 10.5-.25, "Aqui",cex = .8)
-text(-.5, 9.5-.25, "Mono",cex = .8)
-text(-.5, 8.5-.25, "Mam",cex = .8)
-
-text(-.5, 7.5-.5, "Geo",cex = .8)
-text(-.5, 6.5-.5, "Aqui",cex = .8)
-text(-.5, 5.5-.5, "Mono",cex = .8)
-text(-.5, 4.5-.5, "Mam",cex = .8)
-
-text(-.5, 3.5-.75, "Geo",cex = .8)
-text(-.5, 2.5-.75, "Aqui",cex = .8)
-text(-.5, 1.5-.75, "Mono",cex = .8)
-text(-.5, 0.5-.75, "Mam",cex = .8)
-
-text(-1.25, 14, "Ace Discrete",cex = .8,srt=90)
-text(-1.25, 10-.25, "fitContinuous",cex = .8,srt=90)
-text(-1.25, 6-.5, "fitDiscrete",cex = .8,srt=90)
-text(-1.25, 2-.75, "Hansen",cex = .8,srt=90)
-
-dev.off()
-
-#This is the way for the paper
+well <- c("spg", "Rcgmin", "Rvmmin", "bobyqa","L-BFGS-B","nlminb","ucminf","Nelder-Mead","nlm","CG","BFGS","newuoa")
 
 propnew <- matrix(NA,ncol=16,nrow=12)
 for (k in c(1:4)){
@@ -109,7 +29,80 @@ if (k==4){propnew[i,12+j] <- prop[[k]][j,i+1]}
   }
 }
 
+row.names(propnew) <- well
+propnew <- cbind(propnew,c(5,6,9,7,10,2,1,3,8,11,12,4))
+propnew <- propnew[order(propnew[,17]) , ][,c(1:16)]
+
+#old way
+#png("propplot.png")
+#plot(x=c(-1,12),y=c(0,17.75),type="n",axes=FALSE,xlab="",ylab="",main="Proportion Correct")
+#for (y in c(1:4)) {
+#  for (x in c(1:12))  {
+#     rect(xleft=x-1,xright=x,ybottom=17-y,ytop=17-(y+1),col=gray(1-propnew[y,x]))
+#   }
+#}
+
+#for (y in c(5:8)) {
+#  for (x in c(1:12))  {
+#     rect(xleft=x-1,xright=x,ybottom=17-y-0.25,ytop=17-(y+1)-0.25,col=gray(1-propnew[y,x]))
+#   }
+#}
+
+#for (y in c(9:12)) {
+#  for (x in c(1:12))  {
+#     rect(xleft=x-1,xright=x,ybottom=17-y-0.5,ytop=17-(y+1)-0.5,col=gray(1-propnew[y,x]))
+#   }
+#}
+
+#for (y in c(13:16)) {
+#  for (x in c(1:12))  {
+#     rect(xleft=x-1,xright=x,ybottom=17-y-0.75,ytop=17-(y+1)-0.75,col=gray(1-propnew[y,x]))
+#   }
+#}
+
+#text(.5, 16.5, "spg",cex = .8,srt=45)
+#text(1.5, 16.8, "Rcgmin",cex = .8,srt=45)
+#text(2.5, 16.8, "Rvmmin",cex = .8,srt=45)
+#text(3.5, 16.8, "bobyqa",cex = .8,srt=45)
+#text(4.7, 17.05, "L-BFGS-B",cex = .8,srt=45)
+#text(5.5, 16.8, "nlminb",cex = .8,srt=45)
+#text(6.5, 16.8, "ucminf",cex = .8,srt=45)
+#text(7.8, 17.2, "Nelder-Mead",cex = .8,srt=45)
+#text(8.5, 16.5, "nlm",cex = .8,srt=45)
+#text(9.5, 16.5, "CG",cex = .8,srt=45)
+#text(10.5, 16.7, "BFGS",cex = .8,srt=45)
+#text(11.5, 16.8, "newuoa",cex = .8,srt=45)
+
+#text(-.5, 15.5, "Geo",cex = .8)
+#text(-.5, 14.5, "Aqui",cex = .8)
+#text(-.5, 13.5, "Mono",cex = .8)
+#text(-.5, 12.5, "Mam",cex = .8)
+
+#text(-.5, 11.5-.25, "Geo",cex = .8)
+#text(-.5, 10.5-.25, "Aqui",cex = .8)
+#text(-.5, 9.5-.25, "Mono",cex = .8)
+#text(-.5, 8.5-.25, "Mam",cex = .8)
+
+#text(-.5, 7.5-.5, "Geo",cex = .8)
+#text(-.5, 6.5-.5, "Aqui",cex = .8)
+#text(-.5, 5.5-.5, "Mono",cex = .8)
+#text(-.5, 4.5-.5, "Mam",cex = .8)
+
+#text(-.5, 3.5-.75, "Geo",cex = .8)
+#text(-.5, 2.5-.75, "Aqui",cex = .8)
+#text(-.5, 1.5-.75, "Mono",cex = .8)
+#text(-.5, 0.5-.75, "Mam",cex = .8)
+
+#text(-1.25, 14, "Ace Discrete",cex = .8,srt=90)
+#text(-1.25, 10-.25, "fitContinuous",cex = .8,srt=90)
+#text(-1.25, 6-.5, "fitDiscrete",cex = .8,srt=90)
+#text(-1.25, 2-.75, "Hansen",cex = .8,srt=90)
+
+#dev.off()
+
+#This is the way for the paper
 png("propplot.png")
+
 plot(x=c(-1,17),y=c(-1,12),type="n",axes=FALSE,xlab="",ylab="")
 for (x in c(1:4)) {
   for (y in c(1:12))  {
@@ -135,42 +128,46 @@ for (x in c(13:16)) {
    }
 }
 
-text(1, 10.5, "spg",cex = .8,pos=2)
-text(1, 9.5, "Rcgmin",cex = .8,pos=2)
-text(1, 8.5, "Rvmmin",cex = .8,pos=2)
-text(1, 7.5, "bobyqa",cex = .8,pos=2)
-text(1, 6.5, "L-BFGS-B",cex = .8,pos=2)
-text(1, 5.5, "nlminb",cex = .8,pos=2)
-text(1, 4.5, "ucminf",cex = .8,pos=2)
-text(1, 3.5, "Nelder-Mead",cex = .8,pos=2)
-text(1, 2.5, "nlm",cex = .8,pos=2)
-text(1, 1.5, "CG",cex = .8,pos=2)
-text(1, 0.5, "BFGS",cex = .8,pos=2)
-text(1, -0.5, "newuoa",cex = .8,pos=2)
+text(1, 10.5, "ucminf",cex = 1.5,pos=2)
+text(1, 9.5, "nlminb",cex = 1.5,pos=2)
+text(1, 8.5, "Nelder-Mead",cex = 1.5,pos=2)
+text(1, 7.5, "newuoa",cex = 1.5,pos=2)
+text(1, 6.5, "spg",cex = 1.5,pos=2)
+text(1, 5.5, "Rcgmin",cex = 1.5,pos=2)
+text(1, 4.5, "bobyqa",cex = 1.5,pos=2)
+text(1, 3.5, "nlm",cex = 1.5,pos=2)
+text(1, 2.5, "Rvmmin",cex = 1.5,pos=2)
+text(1, 1.5, "L-BFGS-B",cex = 1.5,pos=2)
+text(1, 0.5, "CG",cex = 1.5,pos=2)
+text(1, -0.5, "BFGS",cex = 1.5,pos=2)
 
-text(1.5, 11.25, "Geo",cex = .8,srt=90,pos=3)
-text(2.5, 11.25, "Aqui",cex = .8,srt=90,pos=3)
-text(3.5, 11.3, "Mono",cex = .8,srt=90,pos=3)
-text(4.5, 11.25, "Mam",cex = .8,srt=90,pos=3)
+text(1.5, 11.4, "Geo",cex = 1.5,srt=90,pos=3)
+text(2.5, 11.4, "Aqui",cex = 1.5,srt=90,pos=3)
+text(3.5, 11.4, "Mono",cex = 1.5,srt=90,pos=3)
+text(4.5, 11.4, "Mam",cex = 1.5,srt=90,pos=3)
 
-text(5.5+.25, 11.25, "Geo",cex = .8,srt=90,pos=3)
-text(6.5+.25, 11.25, "Aqui",cex = .8,srt=90,pos=3)
-text(7.5+.25, 11.3, "Mono",cex = .8,srt=90,pos=3)
-text(8.5+.25, 11.25, "Mam",cex = .8,srt=90,pos=3)
+text(5.5+.25, 11.4, "Geo",cex = 1.5,srt=90,pos=3)
+text(6.5+.25, 11.4, "Aqui",cex = 1.5,srt=90,pos=3)
+text(7.5+.25, 11.4, "Mono",cex = 1.5,srt=90,pos=3)
+text(8.5+.25, 11.4, "Mam",cex = 1.5,srt=90,pos=3)
 
-text(9.5+.5, 11.25, "Geo",cex = .8,srt=90,pos=3)
-text(10.5+.5, 11.25, "Aqui",cex = .8,srt=90,pos=3)
-text(11.5+.5, 11.3, "Mono",cex = .8,srt=90,pos=3)
-text(12.5+.5, 11.25, "Mam",cex = .8,srt=90,pos=3)
+text(9.5+.5, 11.4, "Geo",cex = 1.5,srt=90,pos=3)
+text(10.5+.5, 11.4, "Aqui",cex = 1.5,srt=90,pos=3)
+text(11.5+.5, 11.4, "Mono",cex = 1.5,srt=90,pos=3)
+text(12.5+.5, 11.4, "Mam",cex = 1.5,srt=90,pos=3)
 
-text(13.5+.75, 11.25, "Geo",cex = .8,srt=90,pos=3)
-text(14.5+.75, 11.25, "Aqui",cex = .8,srt=90,pos=3)
-text(15.5+.75, 11.3, "Mono",cex = .8,srt=90,pos=3)
-text(16.5+.75, 11.25, "Mam",cex = .8,srt=90,pos=3)
+text(13.5+.75, 11.4, "Geo",cex = 1.5,srt=90,pos=3)
+text(14.5+.75, 11.4, "Aqui",cex = 1.5,srt=90,pos=3)
+text(15.5+.75, 11.4, "Mono",cex = 1.5,srt=90,pos=3)
+text(16.5+.75, 11.4, "Mam",cex = 1.5,srt=90,pos=3)
 
-text(3, 12.35, "Ace Discrete",cex = .8)
-text(7+.25, 12.35, "fitContinuous",cex = .8)
-text(11+.5, 12.35, "fitDiscrete",cex = .8)
-text(15+.75, 12.35, "Hansen",cex = .8)
+text(3, 12.35, "Ace",cex = 1.75)
+text(7+.25, 12.35, "fitContinuous",cex = 1.75)
+text(11+.5, 12.35, "fitDiscrete",cex = 1.75)
+text(15+.75, 12.35, "hansen",cex = 1.75)
 
+text((2.5+3.5)/2,9.5,"default",col="white",cex=2)
+text((6.5+.25+7.5+.25)/2,1.5,"default",col="white",cex=2)
+text((10.5+.5+11.5+.5)/2,1.5,"default",col="black",cex=2)
+text((14.5+.75+15.5+.75)/2,8.5,"default",col="white",cex=2)
 dev.off()
