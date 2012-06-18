6c08b533f7e189edd978ea9d33dea25d3c42820c
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2012-06-18 17:36:42 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phylooptim@795 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ouch/ouch.multi.cts.R
+++ pkg/R/ouch/ouch.multi.cts.R
@@ -1,12 +1,17 @@
 #Make sure to have ouchbackupcts.RData in the correct directory,
 rm(list = ls())
 require(ouch)
+rm(hansen)
 require(optimx)
 require(geiger)
 require(multicore)
-load("ouchbackupcts.RData")
+#load("ouchbackupcts.RData")
+source("ouch.error2.R")
 source("f.ouch.cts.R")
-source("ouch.wrap.x.R")
+source("ouch.wrap2.x.R")
+
+well <- c("spg", "Rcgmin", "Rvmmin", "bobyqa","L-BFGS-B","nlminb","ucminf","Nelder-Mead","nlm","CG","BFGS","newuoa")
+wellt <- c("spg", "Rcgmin", "Rvmmin", "bobyqa","L-BFGS-B",1,1,1,1,1,1,1)
 
 #The following will detect the number of cores you have.
 if(.Platform$OS.type == "windows") {
@@ -33,60 +38,81 @@ detectCores <- function(all.tests = FALSE)
 }
 }
 
-M <- detectCores()                      #Use if you want all cores used (Total # of cores)
-#M <- 2                                   #Use if you want to choose the # of cores to use
-it <- 50  	                         #Number of iterations (at least 2)
+M <- detectCores()                       #Use if you want all cores used (Total # of cores)
+#M <- 2                                  #Use if you want to choose the # of cores to use
+#it <- 2  	                         #Number of iterations (at least 2)
+iter <- 500                              #Number of max itn's (in function)
 z <- length(well)                        #Number of optimizers
-MIN=0.01                                 #min upper value
-MAX=10                                   #max upper value
-strt <- seq(MIN,MAX,length=it)           #Start values
+#MIN=0.01                                #min upper value
+#MAX=10                                  #max upper value
+#strt <- seq(MIN,MAX,length=it)          #Start values
+#strt <- c(0.4857143,4.7671429)
+strt <- list()
+strt[[1]] <- c(0.5,1.5,2.5,3.5,4.5)
+strt[[2]] <- c(0.5,1.5,2.5,3.5,4.5)
+names(strt) <- c("sqrt.alpha","sigma")
+it <- length(strt[[1]])
+
 N <- min(M,it)
 
 a <- ceiling(it / N)
-h <- 1
 m <- 1
 v <- vector("list",N)
-for (b in c(1:N)){v[[b]]<-matrix(NA,nrow=a,ncol=1)}
+
+for (b in c(1:N)){v[[b]]<-vector("list",length(strt))}
+for (b in c(1:N)){for (t in c(1:N)){v[[b]][[t]] <- matrix(NA,nrow=a,ncol=length(strt[[2]]))}}
 for (d in c(1:a)){
   for (j in c(1:N)){
-    if (m > it*z) {break}else{
-    if (h <= 12){
-      if (j < N ){v[[j]][d] <- c(strt[m]);h <- h+1;m <- m+1}else{v[[j]][d] <- c(strt[m]);h<-h+1;m <- m+1;next}
-                 }else{
-      h <- 1
-      if (j < N ){v[[j]][d] <- c(strt[m]);h <- h+1;m <- m+1}else{v[[j]][d] <- c(strt[m]);h<-h+1;m <- m+1;next}}}
+    for (k in c(1:length(strt))){
+       if (k==1) {if (j < N ){v[[k]][[j]][d,] <- strt[[2]]}else{v[[k]][[j]][d,] <- rep(strt[[1]][m],length(strt[[2]]));m <- m+1}}
+       if (k==2) {if (j < N ){v[[k]][[j]][d,] <- strt[[2]]}else{v[[k]][[j]][d,] <- rep(strt[[1]][m],length(strt[[2]]));m <- m+1}}
+    }
   }
 }
 
-oli <- rep(NA,N)
-sum.oli <- rep(NA,N)
-for (b in c(1:N)){v[[b]] <- na.omit(v[[b]])}
-for (b in c(1:N)){oli[b] <- length(v[[b]])}
-for (b in c(1:N)){sum.oli[b] <- sum(oli[b:1])}
+for (i in c(1:N)){
+  for (k in c(1:length(strt[[2]]))){
+    for (l in c(1:a)){
+      if (is.na(v[[i]][[2]][l,k])){v[[i]][[1]][l,k] <- NA}
+    }
+  }
+}
+
+for (b in c(1:N)){for (i in c(1:length(strt))){v[[b]][[i]] <- na.omit(v[[b]][[i]])}}
+
 #DATA TO BE USED (Default is geospiza)
 ##Geospiza data
-data(geospiza)
-name <- c("geo")
-tree <- geospiza$geospiza.tree
-a.trait <- geospiza$geospiza.data
+#data(geospiza)
+#name <- c("geo")
+#tree <- geospiza$geospiza.tree
+#a.trait <- geospiza$geospiza.data
 
 ##Which column of data do you want?
-kk <- 1
+#kk <- 1
 
 ##Aquilegia data
-#name <- c("aqui")
-#tree <- read.tree("Aquilegia.new.tre")
-#a.trait <- read.delim("Aquilegia.traits",row.names=1)
+name <- c("aqui")
+tree <- read.tree("Aquilegia.new.tre")
+a.trait <- read.delim("Aquilegia.traits",row.names=1)
+
+##Which column of data do you want?
+kk <- 2
 
 ##Monocot Data
 #name <- c("mono")
 #tree <- read.tree("BJO.Monocot.tre")
 #a.trait <- read.delim("BJO.monocot_GS",row.names=1)
 
+##Which column of data do you want?
+#kk <- 2
+
 ##Mammal Data
 #name <- c("mam")
 #tree<-read.nexus("mammalChar1.nex")
 #a.trait <- read.csv("mammallogChar1.csv", row.names=1)
+
+##Which column of data do you want?
+#kk <- 1
 
 if (dim(a.trait)[2] == 1){a.trait <- data.frame(a.trait,well=rep(1,length(a.trait)))}
 nc <- name.check(tree,a.trait)
@@ -110,19 +136,41 @@ results <- collect(jobs,wait=TRUE)
 total.time <- as.numeric(proc.time()[3]-b.time[3])/(60)
 
 l <- vector("list",length(well))
+for (i in c(1:length(well))){l[[i]] <- vector("list",2)}
+for (i in c(1:length(well))){l[[i]][[2]] <- vector("list",length(v))}
 names(l) <- well
-for (i in c(1:length(well))){l[[i]] <- matrix(NA,ncol=9,nrow=it);colnames(l[[i]]) <- c("I","lb","ub","T","L","S","A","conv","time")}
+for (i in c(1:length(well))){l[[i]][[1]] <- matrix(NA,ncol=9,nrow=length(strt[[1]])*length(strt[[2]]));colnames(l[[i]][[1]]) <- c("sa.I","sig.I","lb","ub","L","SA","SIG","conv","time")}
+
+oli <- rep(NA,N)
+sum.oli <- rep(NA,N)
+for (i in c(1:N)){oli[i] <- length(results[[i]][[1]][[1]][,1])}
+for (b in c(1:N)){sum.oli[b] <- sum(oli[b:1])}
 
 for (j in c(1:N)){
   for (i in c(1:z)){
     for (d in c(1:oli[j])){
-      if (j == 1 ){l[[i]][d,] <- as.numeric(results[[j]][[i]][d,])}else{
-                   l[[i]][d+sum.oli[j-1],] <- as.numeric(results[[j]][[i]][d,])}
+      if (j == 1 ){l[[i]][[1]][d,] <- as.numeric(results[[j]][[i]][[1]][d,])}else{
+                   l[[i]][[1]][d+sum.oli[j-1],] <- as.numeric(results[[j]][[i]][[1]][d,])}
     }
   }
 }
 
-for (i in c(1:z)){l[[i]] <- l[[i]][order(l[[i]][,1]) , ]}
+for (j in c(1:N)){
+  for (i in c(1:z)){
+    l[[i]][[2]][[j]] <- results[[j]][[i]][[2]]
+  }
+}
+for (i in c(1:z)){l[[i]][[1]] <- l[[i]][[1]][order(l[[i]][[1]][,1]) , ]}
+
+ll <- vector("list",length(well))
+names(ll) <- well
+for (j in c(1:length(well))){for (i in c(1:N)){ll[[j]] <- c(ll[[j]],l[[j]][[2]][[i]])}}
+
+for (i in c(1:length(well))){a <- order(as.numeric(names(ll[[i]])));ll[[i]] <- ll[[i]][a]}
+
+for (i in c(1:z)){l[[i]][[2]] <- ll[[i]]}
+
+ll <- NULL
 
 #Saves the results in the directory
-save.image(paste(getwd(),"/",name,"ouchcts.RData",sep=""))
+save.image(paste(getwd(),"/",name,"ouchctspath.RData",sep=""))
